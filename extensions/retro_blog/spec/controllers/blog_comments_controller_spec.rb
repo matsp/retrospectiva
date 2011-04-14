4562e15e709e46055f97371ffc9902c41a58a750
Julian Bäume <julian@svg4all.de>
2011-04-14 12:15:41 +0200

    make retro_blog spec work again
    at least they are compiling and running now. still some failing, because
    of missing stubs, I guess. Need investigation

--- extensions/retro_blog/spec/controllers/blog_comments_controller_spec.rb
+++ extensions/retro_blog/spec/controllers/blog_comments_controller_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe BlogCommentsController do
-  it_should_behave_like EveryProjectAreaController
+  it_should_behave_like "EveryProjectAreaController"
 
   before do
     @project = permit_access_with_current_project! :name => 'Retro', :wiki_title => 'Retro'
